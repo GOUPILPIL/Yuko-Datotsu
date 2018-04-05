358ccffd471749d1067492c3aa9e4225c8b39737
Goupilpil <vibucho@gmail.com>
2018-04-05 11:19:56 +0200

    018 Event methode from DefaultController to EventController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,14 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Event;
 use AppBundle\Entity\User;use FOS\UserBundle\Model\UserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Utils\GeoHelper;
-use AppBundle\Form\EventFormType;
 
 class DefaultController extends Controller
 {
@@ -22,64 +18,6 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
-    }
-
-    /**
-     * @Route("/event/create", name="eventCreation")
-     */
-    public function eventCreation(Request $request, GeoHelper $geoHelper)
-    {
-        $event = new Event();
-
-        $form = $this->createForm(EventFormType::class, $event);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userID = $this->getUser()->getId();
-            $UserPdo = $this->getDoctrine()->getRepository('AppBundle:User');
-            $UserOjbect = $UserPdo->findOneById($userID);
-
-            $em = $this->getDoctrine()->getManager();
-            $data = $form["address"]->getData();
-
-            $latLong = $geoHelper->getLatLong($data);
-            $latitude = $latLong['latitude'] ? $latLong['latitude'] : '0';
-            $longitude = $latLong['longitude'] ? $latLong['longitude'] : '0';
-
-            $event->setLat($latitude);
-            $event->setLng($longitude);
-
-            $event->setUser($UserOjbect);
-
-            $em->persist($event);
-            $em->flush();
-            return $this->redirectToRoute('homepage');
-        }
-        return $this->render('events/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-
-    }
-
-    /**
-     * @Route("/event", name="event")
-     */
-    public function listAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $dql = "SELECT a FROM AppBundle:Event a ORDER BY a.dateStart ASC";
-        $query = $em->createQuery($dql);
-
-        $paginator = $this->get('knp_paginator');
-        $pagination = $paginator->paginate(
-            $query, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
-            5/*limit per page*/
-        );
-
-        // parameters to template
-        return $this->render('events/events.paginator.html.twig', array('pagination' => $pagination));
     }
 
     /**
@@ -107,73 +45,5 @@ class DefaultController extends Controller
 
     }
 
-    /**
-     * @Route("/event/{event}",options = { "expose" = true }, name="eventView")
-     */
-    public function eventView(request $request,Event $event)
-    {
 
-        $userID = $this->getUser();
-        $userFetched = $event->getUser();
-
-        if($userID == $userFetched)
-        {
-            $isAlive = 1;
-        }else {
-            $isAlive = 0;
-        }
-
-        return $this->render('events/event.view.html.twig', array('event' => $event, 'isAlive' => $isAlive ));
-    }
-
-    /**
-     * @Route("/event/delete/{id}", name="deleteView")
-     */
-    public function deleteView(request $request,Event $event)
-    {
-
-        $userID = $this->getUser();
-        $userFetched = $event->getUser();
-
-        if($userID == $userFetched)
-        {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($event);
-            $em->flush();
-            return new Response("deleted");
-        }
-
-        return new Response($event->getUser(). " + ". $this->getUser());
-    }
-
-    /**
-     * @route("/event/edit/{event}", name="editView", requirements={"id" = "\d+"}))
-     */
-    public function editAction(Request $request, Event $event)
-    {
-        $userID = $this->getUser();
-        $userFetched = $event->getUser();
-
-        if($userID != $userFetched)
-        {
-            return new Response("GTFO");
-        }
-
-        $form = $this->createForm(EventFormType::class, $event);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($event);
-            $em->flush();
-            return $this->redirectToRoute('eventView', array('event'=> $event->getId()));
-        }
-
-        return $this->render('events/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-
-    }
 }
---
+++ src/AppBundle/Controller/EventController.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zephor
+ * Date: 4/5/18
+ * Time: 11:11 AM
+ */
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Event;
+use AppBundle\Entity\User;use FOS\UserBundle\Model\UserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Utils\GeoHelper;
+use AppBundle\Form\EventFormType;
+
+class EventController extends Controller
+{
+    /**
+     * @Route("/event/create", name="eventCreation")
+     */
+    public function eventCreation(Request $request, GeoHelper $geoHelper)
+    {
+        $event = new Event();
+
+        $form = $this->createForm(EventFormType::class, $event);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userID = $this->getUser()->getId();
+            $UserPdo = $this->getDoctrine()->getRepository('AppBundle:User');
+            $UserOjbect = $UserPdo->findOneById($userID);
+
+            $em = $this->getDoctrine()->getManager();
+            $data = $form["address"]->getData();
+
+            $latLong = $geoHelper->getLatLong($data);
+            $latitude = $latLong['latitude'] ? $latLong['latitude'] : '0';
+            $longitude = $latLong['longitude'] ? $latLong['longitude'] : '0';
+
+            $event->setLat($latitude);
+            $event->setLng($longitude);
+
+            $event->setUser($UserOjbect);
+
+            $em->persist($event);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+        return $this->render('events/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/event", name="event")
+     */
+    public function listAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $dql = "SELECT a FROM AppBundle:Event a ORDER BY a.dateStart ASC";
+        $query = $em->createQuery($dql);
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            5/*limit per page*/
+        );
+
+        // parameters to template
+        return $this->render('events/events.paginator.html.twig', array('pagination' => $pagination));
+    }
+
+
+
+    /**
+     * @Route("/event/{event}",options = { "expose" = true }, name="eventView", requirements={"event" = "\d+"})
+     */
+    public function eventView(request $request,Event $event)
+    {
+
+        $userID = $this->getUser();
+        $userFetched = $event->getUser();
+
+        if($userID == $userFetched)
+        {
+            $isAlive = 1;
+        }else {
+            $isAlive = 0;
+        }
+
+        return $this->render('events/event.view.html.twig', array('event' => $event, 'isAlive' => $isAlive ));
+    }
+
+    /**
+     * @Route("/event/delete/{event}", name="deleteView", requirements={"event" = "\d+"})
+     */
+    public function deleteView(request $request,Event $event)
+    {
+
+        $userID = $this->getUser();
+        $userFetched = $event->getUser();
+
+        if($userID == $userFetched)
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($event);
+            $em->flush();
+            return new Response("deleted");
+        }
+
+        return new Response($event->getUser(). " + ". $this->getUser());
+    }
+
+    /**
+     * @route("/event/edit/{event}", name="editView", requirements={"event" = "\d+"}))
+     */
+    public function editAction(Request $request, Event $event)
+    {
+        $userID = $this->getUser();
+        $userFetched = $event->getUser();
+
+        if($userID != $userFetched)
+        {
+            return new Response("GTFO");
+        }
+
+        $form = $this->createForm(EventFormType::class, $event);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+            return $this->redirectToRoute('eventView', array('event'=> $event->getId()));
+        }
+
+        return $this->render('events/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+}
